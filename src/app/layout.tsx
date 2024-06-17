c24f3292495c14d7b7e0648728bf907312fb6e34
Markus Nahesson <markusnahesson@gmail.com>
2024-06-17 21:46:12 +0700

    Link & Navigation, API Route Handlers, Data Fetching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,11 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './navbar'
 import { useState } from 'react'
+import { usePathname } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] })
+
+const disableNavbar = ["/login", "/register"];
 
 export default function RootLayout({
   children,
@@ -14,12 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const [state, setState] = useState(0);
+  const pathname = usePathname();
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Navbar></Navbar> */}
-        {/* <div>Layout {state}</div> */}
-        {/* <button onClick={() => setState(state + 1)}>Klik</button> */}
+        {!disableNavbar.includes(pathname) && <Navbar />}
+        {/* <div>Layout {state}</div>
+        <button onClick={() => setState(state + 1)}>Klik</button> */}
         {children}
       </body>
     </html>
--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,11 +1,39 @@
-type DetailProductPageProps = { params: { slug: string[] } };
+type ProductPageProps = { params: { slug: string[] } };
 
-export default function DetailProductPage(props: DetailProductPageProps) {
+async function getData() {
+    const res = await fetch("https://fakestoreapi.com/products"); 
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+}
+
+export default async function ProductPage(props: ProductPageProps) {
     const { params } = props;
-    console.log(params)
+    const products = await getData();
+    console.log(products);
     return (
-        <div className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1>
+        <div className="grid grid-cols-4 mt-5 place-items-center">
+            {/* <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1> */}
+            {products.length > 0 && products.map((product: any) => (
+            <div key={product.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5">
+                <a href="#">
+                    <img className="p-8 rounded-t-lg object-cover h-96 w-full" src={product.image} alt="product image" />
+                </a>
+                <div className="px-5 pb-5">
+                    <a href="#">
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">{product.title}</h5>
+                    </a>
+                    <div className="flex items-center justify-between mt-3">
+                        <span className="text-3xl font-bold text-gray-900 dark:text-white">$ {product.price}</span>
+                        <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+
+            ))}
             {params.slug && 
                 <>
                     <p>Category : {params.slug[0]}</p>
