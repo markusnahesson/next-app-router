95703d71d17cea90a4e6a1cb5d2f2a7d2239edb7
Markus Nahesson <markusnahesson@gmail.com>
2024-06-18 22:03:12 +0700

    Caching & Revalidating, Loading UI & Error Handling

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -3,13 +3,33 @@ import { NextRequest, NextResponse } from "next/server";
 const data = [
     {
         id: 1,
-        name: "Sepatu Baru 1",
-        price: 100000
+        name: "Nike Air Max 97",
+        price: 2389000,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/3703a3c3-64f0-45be-a06b-b177cc63fda7/air-max-97-shoes-EBZrb8.png"
     },
     {
         id: 2,
-        name: "Sepatu Baru 2",
-        price: 200000
+        name: "Nike Court Vision Low",
+        price: 1199000,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ff3766b5-59dd-4b9e-b755-751a3785d64d/court-vision-low-shoes-nRP2tZ.png"
+    },
+    {
+        id: 3,
+        name: "Nike P-6000",
+        price: 1429000,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/24677ace-081d-4ae4-9ec7-6887c682a40e/p-6000-shoes-QcQbpx.png"
+    },
+    {
+        id: 4,
+        name: "Nike P-6000 Copy",
+        price: 1429000,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/24677ace-081d-4ae4-9ec7-6887c682a40e/p-6000-shoes-QcQbpx.png"
+    },
+    {
+        id: 5,
+        name: "Nike P-6000 Copy 2",
+        price: 1429000,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/24677ace-081d-4ae4-9ec7-6887c682a40e/p-6000-shoes-QcQbpx.png"
     }
 ]
 
--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,7 +1,16 @@
 type ProductPageProps = { params: { slug: string[] } };
 
 async function getData() {
-    const res = await fetch("https://fakestoreapi.com/products"); 
+    // const res = await fetch("https://fakestoreapi.com/products", {
+    //     cache: "no-store",
+    // });
+    const res = await fetch("http://localhost:3000/api/products", {
+        cache: "no-store",
+        next: {
+            tags: ["products"]
+            // revalidate: 30,
+        }
+    }); 
 
     if (!res.ok) {
         throw new Error("Failed to fetch data");
@@ -13,11 +22,11 @@ async function getData() {
 export default async function ProductPage(props: ProductPageProps) {
     const { params } = props;
     const products = await getData();
-    console.log(products);
+    
     return (
         <div className="grid grid-cols-4 mt-5 place-items-center">
             {/* <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1> */}
-            {products.length > 0 && products.map((product: any) => (
+            {products.data.length > 0 && products.data.map((product: any) => (
             <div key={product.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5">
                 <a href="#">
                     <img className="p-8 rounded-t-lg object-cover h-96 w-full" src={product.image} alt="product image" />
